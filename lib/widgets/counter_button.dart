3c9e713ea17a50fe912259e502aa39a765d95e8f
sushmi <contact.wafflehunt@gmail.com>
2026-05-08 17:33:51 +0530

    update widgets

---
+++ lib/widgets/counter_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CounterButton extends StatelessWidget {
+
+  final VoidCallback onPressed;
+
+  const CounterButton({
+    super.key,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      child: Icon(Icons.add),
+    );
+  }
+}
